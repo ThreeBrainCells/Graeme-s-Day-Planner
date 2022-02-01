3cac04f7143251856b7713dfe29b81505d6f6e1d
Graeme Montrose <teentrose@gmail.com>
2022-01-31 18:22:17 -0700

    added faulty code for review

--- Assets/script.js
+++ Assets/script.js
@@ -1,28 +1,4 @@
-// ## User Story
 
-// ```md
-// AS AN employee with a busy schedule
-// I WANT to add important events to a daily planner
-// SO THAT I can manage my time effectively
-// ```
-
-// ## Acceptance Criteria
-
-// ```md
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-// ```
 var saveBtn=$('.saveBtn')
 
 
@@ -36,40 +12,52 @@ setInterval(update, 1000)
 
 //uses moment.js to compare current hour to hour within input blocks, then changes their color.
 function changeTime(){
-    var currentHour = moment().format('hh a');
+    var currentHour = moment().format('hha').toString();
     console.log(currentHour)
 
     $('.input-group').each(function (){
-        var currentBlock = $('.saveBtn').text;
-        console.log(currentBlock);
+        $('.input-group').children().each(function(){
+        var currentBlock = $('span').attr('id')
+            console.log(currentBlock);
         if (currentBlock <= currentHour){
-            $('.input-group').addClass(".past")
+            $('.input-group').addClass(".past").removeClass('present future')
         }else if(currentBlock == currentHour){
-            $('.input-group').addClass(".present")
+            $('.input-group').addClass(".present").removeClass('past future')
         }else{
-            $('.input-group').addClass(".future")
+            $('.input-group').addClass(".future").removeClass('present future')
         }
+        });
+
         //use jQuery to grab the ID of the current block's span with the time string
         //compare currentBlock time with currentHour
         //add css class .past .present or .future depending on comparision results.
     })
 }
-//upon clicking save button, change placeholder text to the typed value, and put said value in local storage.
-function saveTask(){
 
-}
+
+
 //retrieve items from local storage and replace the input's placeholder text with the stored items.
-function getSaved(){
-
-}
+// function getSaved(){
+    
+//     $('.form-control').each(function(){
+//         var oldTask = localStorage.getItem("saved task");
+//         $('.form-control').placeholder.val(oldTask);
+        
+//     })
+// }
 
 //put init function here to trigger other functions.
 function init(){
     // getSaved();
     changeTime();
 };
+//upon clicking save button, change placeholder text to the typed value, and put said value in local storage.
 
-saveBtn.click(saveTask());
+$('.saveBtn').on("click",function(){
+    var savedTask=$(".form-control").val();
+    $('.form-control').placeholder.val(savedTask);
+    localStorage.setItem("saved task", savedTask);
+});
 
 init()
 
